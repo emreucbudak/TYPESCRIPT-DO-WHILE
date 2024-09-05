03ab2ba8a2f9b19c230db5c40d0025ae73ac19ee
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 20:55:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var a = 15;
+while (a > 0) {
+    console.log("Emre Üçbudak");
+    a--;
+}
+var b = 16;
+do {
+    console.log("do-while");
+    b--;
+} while (b > 0);
---
+++ app.ts
@@ -0,0 +1,11 @@
+let a = 15
+while (a>0) {
+    console.log("Emre Üçbudak")
+    a--;
+}
+let b = 16
+do {
+    console.log("do-while")
+    b--
+}
+while (b>0)
